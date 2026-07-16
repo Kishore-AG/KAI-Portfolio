c073625bb241076fd673ccc11ec032413938f9f4
Kishore-AG <kishoreag22@gmail.com>
2026-07-16 23:04:38 +0530

    Updated frontend

--- frontend/admin/services/auth.js
+++ frontend/admin/services/auth.js
@@ -1,4 +1,4 @@
-const API_URL = "https://kai-portfolio-4kbr.onrender.com/auth/login"
+const API_URL = "https://kai-portfolio-4kbr.onrender.com/"
 
 export async function login(username, password) {
 
