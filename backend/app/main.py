7a371fa85a84c04f0bb8392f4d15bd0104023e0a
Kishore-AG <kishoreag22@gmail.com>
2026-07-16 23:16:43 +0530

    Updated frontend cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -54,7 +54,7 @@ app.add_middleware(
         "http://localhost:5500",
         "http://127.0.0.1:3000",
         "http://localhost:3000",
-        "https://6a590eb68cecff5620780c5e--kishore-ag.netlify.app/",
+        "https://6a59178132149a6d2141c4cb--kishore-ag.netlify.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
