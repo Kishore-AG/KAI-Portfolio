8cda656ce55991714c3a16bda9beadcbd61b7a1e
Kishore-AG <kishoreag22@gmail.com>
2026-07-16 23:29:50 +0530

    Updated url

--- backend/app/main.py
+++ backend/app/main.py
@@ -54,7 +54,7 @@ app.add_middleware(
         "http://localhost:5500",
         "http://127.0.0.1:3000",
         "http://localhost:3000",
-        "https://6a59178132149a6d2141c4cb--kishore-ag.netlify.app",
+        "https://kishore-ag.netlify.app/admin/login.html",
     ],
     allow_credentials=True,
     allow_methods=["*"],
