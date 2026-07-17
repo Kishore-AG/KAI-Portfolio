3874bec711722642f6eafce22a0ac97f39ad9c74
Kishore-AG <kishoreag22@gmail.com>
2026-07-17 17:45:09 +0530

    Add Cloudinary

--- backend/core/config.py
+++ backend/core/config.py
@@ -11,9 +11,9 @@ class Settings:
     ACCESS_TOKEN_EXPIRE_MINUTES = int(
         os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60)
     )
-    CLOUDINARY_CLOUD_NAME: str
-    CLOUDINARY_API_KEY: str
-    CLOUDINARY_API_SECRET: str
+    CLOUDINARY_CLOUD_NAME = os.getenv("CLOUDINARY_CLOUD_NAME")
+    CLOUDINARY_API_KEY = os.getenv("CLOUDINARY_API_KEY")
+    CLOUDINARY_API_SECRET = os.getenv("CLOUDINARY_API_SECRET")
 
 
 settings = Settings()
